fd7ca5cd12fa2eaafa4429fa9ceac5447bac8f54
HelenPetukhova <lekar21@yahoo.com>
2025-04-10 13:17:49 +0300

    HomeTask 21 - Rest Api testing against https://jsonplaceholder.typicode.com/posts using RestAssured

---
+++ HomeTask21RestAPI/src/test/java/com/itacademy/aqa/PostResponse.java
@@ -0,0 +1,39 @@
+package com.itacademy.aqa;
+
+public class PostResponse {
+
+    private String body;
+    private String userId;
+    private String id;
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+
+}
